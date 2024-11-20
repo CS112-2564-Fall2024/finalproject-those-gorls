e7ee7998fe3563ac290c134bb9ac8d2998eb1de1
lenice <lenicems06@gmail.com>
2024-11-20 10:53:53 -0800

    commit

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/Bee.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/Bee.java
@@ -1,4 +1,42 @@
 package edu.miracosta.cs112.finalproject.finalproject;
-
 public class Bee {
+    public String name;
+    public int sizeInCm;
+    public String gender;
+
+    //Constructors
+    public Bee(){
+
+    }
+
+    public Bee(String name, int size, String gender){
+
+    }
+    //Getters
+    public String getName(){
+        return name;
+    }
+    public int getSizeInCm() {
+        return sizeInCm;
+    }
+    public String getGender() {
+        return gender;
+    }
+    //Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSizeInCm(int sizeInCm) {
+        this.sizeInCm = sizeInCm;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    //toString
+    public abstract String toString(String name, int sizeInCm, String gender);
+
+    //other methods
+    public abstract lifeSpan;
+
 }
--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/MainViewController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/MainViewController.java
@@ -6,10 +6,14 @@ import javafx.scene.control.Label;
 public class MainViewController {
     @FXML
     private Label welcomeText;
+    private
 
     @FXML
     protected void onYesButtonAction() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    protected void speakerButtonAction(){ }
 
 }
