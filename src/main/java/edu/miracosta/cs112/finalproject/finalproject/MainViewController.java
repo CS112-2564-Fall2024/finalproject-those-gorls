916dc8d0ef1b3b5b06a9583a74aa744610def6a5
lenice <lenicems06@gmail.com>
2024-11-19 21:43:35 -0800

    AHH

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/MainViewController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/MainViewController.java
@@ -3,12 +3,13 @@ package edu.miracosta.cs112.finalproject.finalproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainViewController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onYesButtonAction() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
