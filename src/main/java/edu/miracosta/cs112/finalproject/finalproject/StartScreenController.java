58b83e0f55c3525a4e0b51897374cef40d2ba543
MCCLab4804WD01 <lenicems06@gmail.com>
2024-11-25 13:04:13 -0800

    commit

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/StartScreenController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/StartScreenController.java
@@ -1,26 +1,59 @@
 package edu.miracosta.cs112.finalproject.finalproject;
 
-import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StartScreenController{
 
     @FXML
     private TextField titleTextField;
     @FXML
-    protected void droneBeeChoice(){
-        titleTextField.setText("Welcome to JavaFX Application!");
+    private TextField intructiionsTextField;
+    @FXML
+    private Button droneBeeButton;
+    @FXML
+    private Button workerBeeButton;
+    @FXML
+    private Button queenBeeButton;
+
+     @FXML
+     protected void initialize(){
+         titleTextField.setText("Welcome to: To Bee Or Not To Bee!");
+         intructiionsTextField.setText("    Choose which role of bee you would like to bee beelow!");
+         droneBeeButton.setText("Drone Bee");
+         workerBeeButton.setText("Worker Bee");
+         queenBeeButton.setText("Queen Bee");
+
+     }
+
+    @FXML
+    protected void droneBeeChoice() throws IOException {
+         handleMainScreen();
         DroneBeeActions drone = new DroneBeeActions();
     }
     @FXML
-    protected void workerBeeChoice(){
-
+    protected void workerBeeChoice() throws IOException {
+        handleMainScreen();
     }
     @FXML
-    protected void queenBeeChoice(){
-
+    protected void queenBeeChoice() throws IOException {
+    handleMainScreen();
     }
 
+    @FXML
+    protected void handleMainScreen() throws IOException {
+         Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("To Bee Or Not To Bee");
+        MainViewController mainController = fxmlLoader.getController();
+        stage.show();
+    }
 }
