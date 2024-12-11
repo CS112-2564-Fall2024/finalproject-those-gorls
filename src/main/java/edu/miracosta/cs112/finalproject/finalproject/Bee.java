fc196fb2428b061ce8451a9accc77acf7bfd71b8
MCCLab4804WD01 <lenicems06@gmail.com>
2024-12-11 13:29:55 -0800

    done?

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/Bee.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/Bee.java
@@ -12,13 +12,7 @@ public abstract class Bee {
     private String tasks;
 
     //Constructors
-    public void incrementEventCount() {
-        eventCount++;
-    }
 
-    public int getEventCount() {
-        return eventCount;
-    }
 
     public boolean hasReachedLifespan() {
         return eventCount >= 7;
@@ -88,8 +82,21 @@ public abstract class Bee {
     }
     public void setColor(String color) { this.color = color; }
     public void setTasks(String tasks) { this.tasks = tasks; }
+
+    //Other Methods
+
     /**
-     * method for type identification
+     * This counts the amounts of events that
+     * have gone through after clicking 'yes'
+     * to go to the next event and ends the
+     * game after seven events
+     */
+    public void incrementEventCount() {
+        eventCount++;
+    }
+    /**
+     * method for type of
+     * bee identification
      * @return the identity of the bee
      */
     public String getType() {
@@ -115,12 +122,39 @@ public abstract class Bee {
     }
 
     // abstract method
-
     /**
-     *
-     * @return
+     * This is the abstract method that all
+     * the children classes must have
+     * @return they all return a different lifespan
+     * depending on the type of bee
      */
     public abstract int getLifeSpan();
 
+    /**
+     * This is the inner class for Bee
+     * it has all the constants for
+     * every Bee that inherits from bee class
+     */
+    public class Constants {
+        public static final String DRONE_NAME = "Drone Bee";
+        public static final int DRONE_SIZE = 2;
+        public static final String DRONE_GENDER = "Male";
+        public static final int DRONE_LIFESPAN = 30;
+        public static final String DRONE_COLOR = "Yellow";
+        public static final String DRONE_TASKS = "\n-Mate with queen \n-Die";
 
+        public static final String WORKER_NAME = "Worker Bee";
+        public static final int WORKER_SIZE = 1;
+        public static final String WORKER_GENDER = "Female";
+        public static final int WORKER_LIFESPAN = 60;
+        public static final String WORKER_COLOR = "Yellow";
+        public static final String WORKER_TASKS = "\n-Take care of queen \n-Collect nectar \n-Collect Pollen \n-Nurse larvae \n-Feed drones \n-Protect hive";
+
+        public static final String QUEEN_NAME = "Queen Bee";
+        public static final int QUEEN_SIZE = 3;
+        public static final String QUEEN_GENDER = "Female";
+        public static final int QUEEN_LIFESPAN = 1825; // Approx. 5 years
+        public static final String QUEEN_COLOR = "Orangish-yellow";
+        public static final String QUEEN_TASKS = "\n-Lay eggs \n-Watch over hive \n-Eat";
+    }
 }
